41688b99dd0803ec8a47d3ae3c02a43fb1ce15eb
piupiujk <docs4p@yandex.ru>
2024-01-25 07:13:28 +0500

    Main page is done, get pages index and about

--- main/views.py
+++ main/views.py
@@ -4,12 +4,17 @@ from django.shortcuts import HttpResponse
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - HOME'
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About Page')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Lorem123'
+    }
+    return render(request, 'main/about.html', context)
